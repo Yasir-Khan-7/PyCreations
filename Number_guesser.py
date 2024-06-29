f2355552c93161a56175e55c94979e3f683e8a88
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-06-29 20:24:52 +0500

    updated

--- Number_guesser.py
+++ Number_guesser.py
@@ -4,19 +4,19 @@ import random
 
 def Number_guesser(range_of_no,random_no):
     guesses = 0
-
-    print(f"random number was: {random_no}")
+    print(f"the range of number is: {range_of_no}")
+   
     
     while True:
         number = input("Enter number to Guess: ")
         guesses=guesses+1
         if random_no==int(number):
-            print(f"you got it in {guesses}")
+            print(f"you got it in {guesses} guesses")
             exit()
         else:  
             print("try again")
             continue
          
-range_of_no =6
-random_no = random.randint(1,range_of_no)       
+range_of_no =input("enter Range of number: ")
+random_no = random.randint(1,int(range_of_no))       
 Number_guesser(range_of_no,random_no)
