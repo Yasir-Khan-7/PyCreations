9726cd4b1381020698991c46400e3cc4038ab9f8
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-07-20 17:05:01 +0500

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,4 +1,32 @@
 #This is adventure game where user  will enter into an adventure and explore crazy things and places
 name = input("Enter your name:  ")
-print("Welcome you to the  'LOST TREASURE'  you have an old map that leads to  a hidden treasure in a forest: ")
+print(f"{name} Welcome you to the  'LOST TREASURE'  you have an old map that leads to  a hidden treasure in a forest: ")
 
+while True:
+    answer = input("Do you want to enter into the adventure: (y/n) ").lower()
+    if answer =='y':
+        print("Yeah! so here you go choose wisely ")
+        answer  = input("Which path will you take left or right: ").lower()
+        
+        if answer == 'left':
+            print("You encounter a river with a rickety bridge.")
+
+            answer = input("Do you want to cross the bridge? (y/n) ").lower()
+            
+            if answer =='y':
+                print("You see a cave entrance hidden behind some bushes.")
+                answer = input("Do you want to enter into the cave? (y/n): ")
+                
+                if answer=='y':
+                    pass
+        else:
+            answer == 'right'
+            print("you fall into a hidden pit and the game ends.")
+            break
+   
+    elif answer =='n':
+        print("your adventure ends here :) good bye! ")
+        break
+    else:
+        print("please enter 'y' to enter into adventure or 'n' to  end the adventure! ")
+        continue
