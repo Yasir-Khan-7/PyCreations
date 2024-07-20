23c25c2cbf097132c01af9e98160154e7e0d0179
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-07-20 16:08:52 +0500

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,1 +1,4 @@
 #This is adventure game where user  will enter into an adventure and explore crazy things and places
+name = input("Enter your name:  ")
+print("Welcome you to the  'LOST TREASURE'  you have an old map that leads to  a hidden treasure in a forest: ")
+
