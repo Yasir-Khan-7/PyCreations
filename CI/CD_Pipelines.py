3a37ad884c4202076d3e44a6b6a4ccbc433b2fd7
yasir-sideline <yasir@sideline.agency>
2025-10-16 12:56:14 +0500

    added CI/CD learning file

---
+++ CI/CD_Pipelines.py
@@ -0,0 +1,15 @@
+#in this will be learnings related to CI/CD Pipelines
+# CI/CD stands for Continuous Integration and Continuous Deployment/Delivery.
+# learning about git and github actions
+
+# SSH --> connecting multiple github accounts ussing SSH keys
+
+# steps:
+# first i create a directory "ssh"
+# so next command "ssh-keygen -t ed25519 -C "yasirkhandps7@gmail.com"
+# ssh-keygen - is keygenerator
+# -t ed25519 is the type of encryption algorithm
+# -C is comment and label with email address
+# then i added the "ssh-add ~/.ssh/id_ed25519"
+
+#then added the key in github account settings
